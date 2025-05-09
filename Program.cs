1b47e2564fac9d70d3e2951518565e7071a0b27d
sh0rtener <domarlaid2@mail.ru>
2025-05-09 15:46:55 +0300

    base - добавил файлы проекта

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class Algoritms
+{
+    private int[] array = GenerateSortedArray(1000);
+    private int value = 78;
+
+    [Benchmark(Baseline = true)]
+    public int FindIndexLinear()
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == value)
+                return i;
+        }
+
+        return -1;
+    }
+
+    [Benchmark]
+    public int FindIndexBinary()
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (array[mid] < value)
+            {
+                left = mid + 1;
+            }
+            if (array[mid] > value)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return array[mid];
+            }
+        }
+
+        return -1;
+    }
+
+    static int[] GenerateSortedArray(int length)
+    {
+        int[] array = new int[length];
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            array[i] = i + 1;
+        }
+
+        return array;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Algoritms>();
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Заметка о способе оценки производительности на C# с помощью BenchmarkDotNet
+(nuget - https://www.nuget.org/packages/BenchmarkDotNet/)<br/>
+
+### Необходимые понятия для понимания:
+- Mean - среднее время выполнения
+- StdDev - число отклонения оценки
+- Error - ошибки оценки от 99.9
+- Ratio - оценка улучшения или ухудшения по отношению к BaseLine = true в процентах
+- Allocated - сколько выделено памяти
+
+
+### Пример
+
+```
+int[] array = GenerateSortedArray(1000);
+int value = 78;
+
+| Method          | Mean      | Error     | StdDev    | Ratio | RatioSD | Allocated | Alloc Ratio |
+|---------------- |----------:|----------:|----------:|------:|--------:|----------:|------------:|
+| FindIndexBinary |  3.393 ns | 0.0196 ns | 0.0163 ns |  0.08 |    0.00 |         - |          NA |
+| FindIndexLinear | 43.665 ns | 0.7275 ns | 0.6449 ns |  1.00 |    0.02 |         - |          NA |
+```
+
+Эту таблицу можно расшифровать так:
+- Метод FindIndexBinary выполняется на ~40ns быстрее, чем метод FindIndexLinear
+- Число отклонения оценки у метода FindIndexBinary равено 0.0196 ns (погрешность)
+- Т.к метод FindIndexLinear является Baseline = true, то относительно этого метода метод FindIndexBinary лучше на 8%
